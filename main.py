8e4dfa188ecb7992602d1fb52b967ed11bc65027
Bart Coppens <kde@bartcoppens.be>
2020-09-20 16:21:02 +0200

    Fetch student publications

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.chrome.webdriver import WebDriver
 from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.ui import Select
 import sys
 
 # Set-up the logger.
@@ -130,6 +131,103 @@ def download_documents(driver: WebDriver, course: str):
     os.rename(out_file, new_file)
     logging.info(f"Saved {new_name}")
 
+# From https://selenium-python.readthedocs.io/waits.html
+class element_has_css_class(object):
+  """An expectation for checking that an element has a particular css class.
+
+  locator - used to find the element
+  returns the WebElement once it has the particular css class
+  """
+  def __init__(self, locator, css_class):
+    self.locator = locator
+    self.css_class = css_class
+
+  def __call__(self, driver):
+    element = driver.find_element(*self.locator)   # Finding the referenced element
+    if self.css_class in element.get_attribute("class"):
+        return element
+    else:
+        return False
+
+def download_student_publications(driver: WebDriver, course: str):
+    # Browse to the home directory.
+    driver.get(course)
+    sleep = WebDriverWait(driver, 10)
+    sleep.until(lambda d: course in d.current_url)
+
+    links = driver.find_elements_by_tag_name('a')
+    present = False
+    for link in links:
+        href = link.get_attribute('href')
+        if href is not None and 'student_publication' in href:
+            color = link.value_of_css_property("color")
+            print (color)
+            if "rgba(30, 100, 200, 1)" in str(color):
+                present = True
+                break
+    if not present:
+        return
+
+    logging.info("Found student publications")
+
+    files = course.replace("course_home/course_home.php", "student_publication/index.php")
+    driver.get(files)
+    sleep = WebDriverWait(driver, 10)
+    sleep.until(lambda d: files in d.current_url)
+
+    # Click the zip link.
+    links = driver.find_elements_by_tag_name('input')
+    id_link = None
+    for link in links:
+        id = link.get_attribute('id')
+        print (id)
+        if id is not None and "select_all_none_actions_top" in id:
+            id_link = link
+            break
+
+    if not id_link:
+        logging.error("id-link not found :(")
+        exit(1)
+
+    id_link.click()
+
+    #sleep = WebDriverWait(driver, 10)
+    #element = sleep.until(element_has_css_class((By.ID, 'select_all_none_actions'), "multiple_actions_checkbox_checked"))
+
+    selects = driver.find_elements_by_tag_name('select')
+    dropdown = None
+    for select in selects:
+        name = select.get_attribute('name')
+        if name is not None and "multiple_actions" in name:
+            dropdown = select
+            break
+
+    if not dropdown:
+        logging.error("dropdown not found, probably a visible student publications without submissions!")
+        return
+
+    Select(dropdown).select_by_visible_text("Download")
+
+    inputs = driver.find_elements_by_tag_name('input')
+    submit = None
+    for input in inputs:
+        id = input.get_attribute('id')
+        if id is not None and "multiple_actions_submit" in id:
+            submit = input
+            break
+
+    if not submit:
+        logging.error("submit not found :(")
+        exit(1)
+
+    submit.click()
+
+    driver.switch_to_alert().accept()
+
+    # These files appear in out_dir/ under the name <CourseCode>-<Name of User>-studentpublications.zip
+    # Given that I can't easily redirect these files in Selenium(?), just leave it
+
+
 
 if __name__ == '__main__':
     # Validate arguments.
@@ -167,5 +265,6 @@ if __name__ == '__main__':
     for ci, course in enumerate(courses):
         logging.info(f"Downloading {ci + 1}/{len(courses)}")
         download_documents(driver, course)
+        download_student_publications(driver, course)
 
     logging.info("Done!")
